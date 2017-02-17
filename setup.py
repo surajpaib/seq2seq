9ccc89f16f1e2c7532e5ac3853d043f80c3e2adf
Suraj Pai <surajballambat@gmail.com>
2017-02-18 04:08:21 +0530

    f

--- setup.py
+++ setup.py
@@ -16,5 +16,5 @@ setup(
       license='GNU GPL v2',
       install_requires=install_requires,
       packages=find_packages(),
-      dependency_links=['https://github.com/farizrahman4u/recurrentshop#egg=recurrentshop-0.0.1']
+      dependency_links=['https://github.com/datalogai/recurrentshop#egg=recurrentshop-0.0.1']
 )
